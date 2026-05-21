3dfaf118bd417ac55628bedb67bdb55003e2ea41
Ducbaynao <phamducyb2004@gmail.com>
2026-05-22 00:21:02 +0700

    first commit

---
+++ Android/src/main/java/com/example/Android/userinteraction/entity/UserMovieInteraction.java
@@ -0,0 +1,54 @@
+package com.example.Android.userinteraction.entity;
+
+import java.time.LocalDateTime;
+
+import com.example.Android.Models.Movie;
+import com.example.Android.Models.User;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user_movie_interactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserMovieInteraction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @Column(name = "type", length = 30)
+    private String type;
+
+    @Column(name = "score")
+    private Integer score;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
